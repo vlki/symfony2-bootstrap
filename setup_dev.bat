201871fe02b4fe4a3b6afb07e63f2e786b2e17d3
Jan Vlcek <vlki@vlki.cz>
2011-10-23 18:36:34 +0200

    added setup_dev scripts for unix & windows

---
+++ setup_dev.bat
@@ -0,0 +1,14 @@
+@echo off
+
+:: Drop and recreate the development database
+php app/console doctrine:database:drop --force
+php app/console doctrine:database:create
+
+:: Load the current schema into the development database
+php app/console doctrine:migrations:migrate --no-interaction
+
+:: Load testing data
+php app/console doctrine:fixtures:load
+
+:: Purge the application cache
+php app/console cache:clear
